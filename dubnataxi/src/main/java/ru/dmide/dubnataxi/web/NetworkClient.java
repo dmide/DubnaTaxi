54a6c64f9dd44b78f263debab24c43889b53a8b5
Dmide <revdmide@gmail.com>
2016-06-13 17:55:37 +0300

    network cache issue fixed

--- dubnataxi/src/main/java/ru/dmide/dubnataxi/web/NetworkClient.java
+++ dubnataxi/src/main/java/ru/dmide/dubnataxi/web/NetworkClient.java
@@ -1,37 +1,61 @@
 package ru.dmide.dubnataxi.web;
 
+import android.util.Log;
+
 import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Cache;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
 import ru.dmide.dubnataxi.ModelFragment;
 
 public class NetworkClient {
+    private static final String TAG = "NetworkClient";
+
     private final WebAPI api;
     private final ModelFragment modelFragment;
-    private final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
-        @Override public Response intercept(Chain chain) throws IOException {
+
+    private final Interceptor REWRITE_RESPONSE_INTERCEPTOR = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
             Response originalResponse = chain.proceed(chain.request());
-            if (modelFragment.isOnline()) {
-                int maxAge = 60; // read from cache for 1 minute
+            String cacheControl = originalResponse.header("Cache-Control");
+
+            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                    cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")) {
                 return originalResponse.newBuilder()
-                        .header("Cache-Control", "public, max-age=" + maxAge)
+                        .header("Cache-Control", "public, max-age=" + 10)
                         .build();
             } else {
-                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
-                return originalResponse.newBuilder()
-                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-                        .build();
+                return originalResponse;
             }
         }
     };
 
-    public NetworkClient(ModelFragment modelFragment, String contentUrl){
+    private final Interceptor OFFLINE_INTERCEPTOR = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+
+            if (!modelFragment.isOnline()) {
+                Log.d(TAG, "rewriting request");
+
+                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
+                request = request.newBuilder()
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                        .build();
+            }
+
+            return chain.proceed(request);
+        }
+    };
+
+    public NetworkClient(ModelFragment modelFragment, String contentUrl) {
         this.modelFragment = modelFragment;
 
         //setup cache
@@ -40,7 +64,8 @@ public class NetworkClient {
         Cache cache = new Cache(httpCacheDirectory, cacheSize);
 
         OkHttpClient client = new OkHttpClient.Builder()
-                .addNetworkInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR)
+                .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                .addInterceptor(OFFLINE_INTERCEPTOR)
                 .cache(cache)
                 .build();
 
